af3e061726dbb34210ddb6e2ea92be19c8d0906f
mohitsk26 <156286494+mohitsk26@users.noreply.github.com>
2026-04-05 11:47:26 +0530

    Create config.py for application configuration
    Added configuration settings for API keys, models, chunking, and paths.

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# API KEYS
+
+GROQ_API_KEY = os.getenv("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")
+
+# MODEL CONFIG
+# ========================
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+LLM_MODEL = "llama3-8b-8192"
+
+
+# CHUNKING CONFIG
+# ========================
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
+
+# RETRIEVAL CONFIG
+
+TOP_K = 3
+
+# PATHS
+# ========================
+UPLOAD_DIR = "data/uploads"
+FAISS_INDEX_PATH = "vectorstore/faiss_index"
